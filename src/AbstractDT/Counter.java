188285c992d26bb31ef3e793e28c8b0296ccc7df
ShaguftaS <shaguftha.96@gmail.com>
2025-12-14 13:33:32 +0530

    First version

--- src/AbstractDT/Counter.java
+++ src/AbstractDT/Counter.java
@@ -1,4 +1,33 @@
 package AbstractDT;
 
 public class Counter {
+
+    String name = null;
+    int value = 0;
+
+    public Counter(String str) {
+
+        this.name = str;
+
+    }
+
+    public void increment(){
+
+        value++;
+
+    }
+
+    public int getCurrentValue(){
+
+        return value;
+
+    }
+
+    public String toString(){
+
+        return name + ": " + value;
+
+    }
+
+
 }
