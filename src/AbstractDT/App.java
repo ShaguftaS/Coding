869ded1938b2560b3b51c0039848db8448172c4b
ShaguftaS <shaguftha.96@gmail.com>
2025-12-14 13:36:03 +0530

    First version

---
+++ src/AbstractDT/App.java
@@ -0,0 +1,17 @@
+package AbstractDT;
+
+public class App {
+
+    public static void main(String[] args) {
+        Counter myCount = new Counter("myCounter");
+        myCount.increment();
+        myCount.increment();
+        myCount.increment();
+        myCount.increment();
+
+        System.out.println(myCount.getCurrentValue());
+
+    }
+
+
+}
