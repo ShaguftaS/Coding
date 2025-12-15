cbb943ffcab239f74d6ddebdd1e9b509cd160a41
ShaguftaS <shaguftha.96@gmail.com>
2025-12-15 15:37:37 +0530

    Stacks

---
+++ src/ds/stack/App.java
@@ -0,0 +1,19 @@
+package ds.stack;
+
+public class App {
+
+    public static void main(String[] args) {
+        Stack stk = new Stack(10);
+        stk.push(20);
+        stk.push(40);
+        stk.push(60);
+        stk.push(80);
+        stk.push(100);
+
+        while(!stk.isEmpty()){
+            long val = stk.pop();
+            System.out.println(val);
+        }
+
+    }
+}
