0a2f32dbb685e1f43eba49fae184f5a9862a6a04
ShaguftaS <shaguftha.96@gmail.com>
2026-02-19 23:06:59 +0530

    Array exercise1

---
+++ src/Exception/Employee.java
@@ -0,0 +1,26 @@
+package Exception;
+
+public class Employee {
+    public void validateAge(int age){
+        if(age<0){
+            throw new IllegalArgumentException("Age can't be negative");
+        }
+        System.out.println("Age is valid"+ age);
+
+    }
+
+    public static void main(String[] args){
+        Employee emp = new Employee();
+
+        try{
+            emp.validateAge(-56);
+
+        }catch(IllegalArgumentException e){
+            System.err.println("Error main:" + e.getMessage());
+        }
+
+
+    }
+
+
+}
