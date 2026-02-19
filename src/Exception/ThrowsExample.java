8868fc30ac95e62d4777ad608d1d6acf461038da
ShaguftaS <shaguftha.96@gmail.com>
2026-02-19 23:15:54 +0530

    Array exercise1

---
+++ src/Exception/ThrowsExample.java
@@ -0,0 +1,32 @@
+package Exception;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class ThrowsExample {
+
+    public void readFile(String filename){
+
+        FileReader read = new FileReader(filename);
+        read.close();
+
+    }
+
+    public static void main(String[] args){
+        ThrowsExample th = new ThrowsExample();
+
+        try{
+            th.readFile("nonesistent.txt");
+        }catch(FileNotFoundException e){
+            System.err.println("File not found "+e.getMessage());
+        }
+        catch(IOException e){
+            System.err.println("IOError "+e.getMessage());
+        }
+
+    }
+
+
+
+}
