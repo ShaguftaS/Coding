779592d8377b38fe3ca27747557d76c4b46d390b
ShaguftaS <shaguftha.96@gmail.com>
2026-02-22 22:10:36 +0530

    Array exercise1

---
+++ src/Collections/HashMapEx.java
@@ -0,0 +1,19 @@
+package Collections;
+
+import java.util.HashMap;
+
+public class HashMapEx {
+
+    public static void main(String[] args){
+
+        HashMap<String, Integer> marks = new HashMap<>();
+
+        marks.put("Happy", 33);
+        marks.put("Shagufta", 34);
+        marks.put("Syeda", 35);
+
+        System.out.println(marks.get("Shagufta"));
+
+
+    }
+}
