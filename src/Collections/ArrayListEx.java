144bd89e95fe8b1183f01bb3cb3b6b37a3a39aad
ShaguftaS <shaguftha.96@gmail.com>
2026-02-22 15:10:13 +0530

    Array exercise1

---
+++ src/Collections/ArrayListEx.java
@@ -0,0 +1,18 @@
+package Collections;
+
+import java.util.ArrayList;
+
+public class ArrayListEx {
+
+    public static void main(String[] args){
+
+        ArrayList<Integer> nums = new ArrayList<>();
+
+        nums.add(10);
+        nums.add(20);
+        nums.add(30);
+
+        System.out.println(nums.get(2));
+
+    }
+}
