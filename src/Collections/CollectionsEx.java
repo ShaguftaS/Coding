71c903db74acd67a55ec28e9db8572cd95bd6f76
ShaguftaS <shaguftha.96@gmail.com>
2026-02-22 22:37:30 +0530

    LinkedList

---
+++ src/Collections/CollectionsEx.java
@@ -0,0 +1,23 @@
+package Collections;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class CollectionsEx {
+
+    public static void main(String[] args){
+
+        ArrayList<Integer> nums = new ArrayList<>();
+
+        nums.add(10);
+        nums.add(50);
+        nums.add(20);
+        nums.add(60);
+        nums.add(30);
+
+        Collections.sort(nums);
+        System.out.println(nums);
+
+    }
+
+}
