a1eb64f73f0409a8492ca079f441640a5207008b
ShaguftaS <shaguftha.96@gmail.com>
2026-03-01 18:46:18 +0530

    TypeSafetyGenerics

---
+++ src/Generics/TypeSafetyEx.java
@@ -0,0 +1,22 @@
+package Generics;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TypeSafetyEx {
+
+    public static void main(String[] args){
+
+        String name = "Shagufta Syeda";
+        //name = 01;
+
+
+        List<Integer> nums = new ArrayList<>();
+
+        nums.add(10);
+
+        nums.add("String");
+
+
+    }
+}
