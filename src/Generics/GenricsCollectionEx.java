8932e02e64b25c2f09682f68276e3f2cf8af2af5
ShaguftaS <shaguftha.96@gmail.com>
2026-03-01 18:42:16 +0530

    GenricsCollectionEx

---
+++ src/Generics/GenricsCollectionEx.java
@@ -0,0 +1,27 @@
+package Generics;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GenricsCollectionEx {
+
+
+    public static void main(String[] args){
+
+        List<Integer> nums = new ArrayList<>();
+
+        nums.add(10);
+        nums.add(20);
+
+        List<String> nums1 = new ArrayList<>();
+
+        nums1.add("Shagufta");
+        nums1.add("Syeda");
+
+    }
+
+
+
+
+
+}
