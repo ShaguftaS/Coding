d355357be9ebb2569ac6aa16f7964993471df22d
ShaguftaS <shaguftha.96@gmail.com>
2026-03-01 19:21:04 +0530

    LambdaExpression

---
+++ src/LambdaExpression/ArrayListLambdaEx.java
@@ -0,0 +1,21 @@
+package LambdaExpression;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayListLambdaEx {
+
+    public static void main(String[] args){
+
+        List<String> list = new ArrayList<>();
+        list.add("Apple");
+        list.add("Banana");
+        list.add("Orange");
+
+        for(String fruit : list){
+            System.out.println(fruit);
+        }
+
+
+    }
+}
