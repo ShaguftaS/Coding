45389b7369937595eb322f20313bc53871791042
ShaguftaS <shaguftha.96@gmail.com>
2026-03-14 22:22:42 +0530

    WithLambdaExpression

--- src/ArrayCodingProblems/SumOfAllElementsArray.java
+++ src/ArrayCodingProblems/SumOfAllElementsArray.java
@@ -4,7 +4,7 @@ public class SumOfAllElementsArray {
 
     public static void main(String[] args){
 
-        int[] num = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int[] num = {1, 2, 3, 4, 5};
         int sum = arraySummation(num);
         System.out.println(sum);
 
@@ -12,13 +12,12 @@ public class SumOfAllElementsArray {
 
     private static int arraySummation(int[] num) {
         int sum = 0;
+
         for( int i : num){
-
             sum += i;
-
         }
-        return sum ;
 
+        return sum ;
     }
 
 
