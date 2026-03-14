aa9f9d3ac4dae33d3dd9156fb0ce2f48d351243a
ShaguftaS <shaguftha.96@gmail.com>
2026-03-14 22:31:45 +0530

    WithLambdaExpression

--- src/ArrayCodingProblems/AverageOfArray.java
+++ src/ArrayCodingProblems/AverageOfArray.java
@@ -5,13 +5,13 @@ public class AverageOfArray {
     public static void main(String[] args){
 
         int[] num = {1, 2, 3, 4, 5 };
-        float avg = findAverage(num);
+        double avg = findAverage(num);
         System.out.println("Average is: " + avg);
 
     }
-    public static float findAverage(int[] arr){
+    public static double findAverage(int[] arr){
 
-        float sum = 0, avg = 0;
+        double sum = 0, avg = 0;
         int len = arr.length;
         for (int i : arr){
             sum += i;
