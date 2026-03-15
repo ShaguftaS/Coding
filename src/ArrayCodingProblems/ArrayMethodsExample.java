ff30e4e10728a83701d9ed17127f1aced8a794be
ShaguftaS <shaguftha.96@gmail.com>
2026-03-15 15:30:39 +0530

    WithLambdaExpression

--- src/ArrayCodingProblems/ArrayMethodsExample.java
+++ src/ArrayCodingProblems/ArrayMethodsExample.java
@@ -23,5 +23,10 @@ public class ArrayMethodsExample {
         int[] newArray = Arrays.copyOf(array, 7);
         System.out.println(Arrays.toString(newArray));
 
+        int[] arr1 = { 1, 2,3 };
+        int[] arr2 = { 1, 2,3 };
+
+        boolean isEqual = Arrays.equals(arr1, arr2);
+        System.out.println(isEqual);
     }
 }
