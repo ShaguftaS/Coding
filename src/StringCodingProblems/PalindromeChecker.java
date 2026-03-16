30623afc9babb79cfb5b004d17dcb8ee59dd59c8
ShaguftaS <shaguftha.96@gmail.com>
2026-03-16 22:37:25 +0530

    WithLambdaExpression

--- src/StringCodingProblems/PalindromeChecker.java
+++ src/StringCodingProblems/PalindromeChecker.java
@@ -14,17 +14,18 @@ public class PalindromeChecker {
 
     public static boolean isPalindrome(String p1){
 
+        if(p1 == null){
+            return false;
+        }
         StringBuilder sb = new StringBuilder();
-
 
         for(int i = p1.length() - 1; i >=0; i--){
 
             sb.append(p1.charAt(i));
-            System.out.println(sb);
 
         }
 
-        if( sb.equals(p1)){
+        if( p1.equals(sb.toString())){
             return true;
         }
         else{
