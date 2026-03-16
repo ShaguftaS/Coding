7e46ff0937b6f683907643e108e2e1bc382ea9b2
ShaguftaS <shaguftha.96@gmail.com>
2026-03-16 22:45:04 +0530

    WithLambdaExpression

---
+++ src/StringCodingProblems/StringExample.java
@@ -0,0 +1,25 @@
+package StringCodingProblems;
+
+public class StringExample {
+    public static void main(String[] args){
+
+        String s1 = "Shagufta";
+        String s2 = "Syeda";
+
+
+        // String length
+        int l1 = s1.length();
+        int l2 = s2.length();
+        System.out.println("Length of s1 " + l1 + " Length of s2 " + l2);
+
+        //Concatenation
+        String res = s1.concat(" , " + s2);
+        System.out.println(res);
+
+
+
+    }
+
+
+
+}
