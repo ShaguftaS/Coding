7a2aa24c54ce65e43626bc857505e731a5224ed3
ShaguftaS <shaguftha.96@gmail.com>
2026-03-17 23:19:39 +0530

    WithLambdaExpression

--- src/NumberCodingProblems/PrimeNumber.java
+++ src/NumberCodingProblems/PrimeNumber.java
@@ -10,7 +10,11 @@ public class PrimeNumber {
     }
     public static boolean isPrime(int n){
 
-        for(int i =2; i <=n; i++){
+        if(n <= 0){
+            return false;
+        }
+
+        for(int i =2; i <=n / 2; i++){
             if(n % i != 0){
 
                 return true;
