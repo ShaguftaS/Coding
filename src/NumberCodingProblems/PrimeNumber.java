86575f25b03cb9fd2548e19a1d9c5ca1419d6c32
ShaguftaS <shaguftha.96@gmail.com>
2026-03-17 23:18:15 +0530

    WithLambdaExpression

---
+++ src/NumberCodingProblems/PrimeNumber.java
@@ -0,0 +1,23 @@
+package NumberCodingProblems;
+
+public class PrimeNumber {
+
+    public static void main(String[] args){
+
+        int n = 17;
+        System.out.println(isPrime(n));
+
+    }
+    public static boolean isPrime(int n){
+
+        for(int i =2; i <=n; i++){
+            if(n % i != 0){
+
+                return true;
+            }
+        }
+        return false;
+
+    }
+
+}
